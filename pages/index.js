a0e205322a179cafab57e272c1fdf9040ec9f482
Oli Fog <oliver@fogelin.com>
2022-07-29 13:32:29 -0400

    small layout adjustements

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,18 @@ import Layout from "/lib/Layout"
 export default function Home() {
   return (
     <Layout>
-      <span className="pb-4 font-caveat text-3xl text-fogDark">Hi, I&apos;m</span>
-      <span className="text-fogDark font-syncopate text-4xl">
-        <span className="text-fogGold">OLI</span>
-        VER FOGELIN
-      </span>
-      <div className="grid grid-cols-2 w-full max-w-md h-64">
-        <div className="bg-red-700 w-full"></div>
-        <div className="bg-red-200 w-full"></div>
+      <div className="flex bg-fogWhite z-20 flex-col items-center pb-8 w-full">
+        <span className="pb-4 font-caveat text-3xl text-fogDark">Hi, I&apos;m</span>
+        <span className="text-fogDark font-syncopate text-4xl pb-8">
+          <span className="text-fogGold">OLI</span>
+          VER FOGELIN
+        </span>
+        <div className="grid grid-cols-2 w-full max-w-md h-64">
+          <div className="bg-red-700 w-full"></div>
+          <div className="bg-red-200 w-full"></div>
+        </div>
+      </div>
+      <div className="bg-fogDark w-full h-64 z-10 relative">
       </div>
     </Layout>
   )
